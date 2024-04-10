5928dae8b324a8b3b1da6021dd7bfcfa42f24a24
lazysharkdev <lazysharkdev@gmail.com>
2024-04-10 10:17:14 +0700

    Intro to webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist", "assets"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map", // Add this option for source mapping
+};
